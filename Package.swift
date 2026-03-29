78f2a6b88fe2ba2d5ad67da4d8a85c88cd9657ba
marchidalgo <marc@theleftbit.com>
2026-03-29 12:52:29 +0200

    Delete SKIP_ENABLE

--- Package.swift
+++ Package.swift
@@ -4,8 +4,6 @@
 import PackageDescription
 import Foundation
 
-let skipIsEnabled = (ProcessInfo.processInfo.environment["SKIP_ENABLED"] != nil)
-
 let applePlatforms = TargetDependencyCondition.when(
     platforms: [
         .iOS,
@@ -21,35 +19,22 @@ var packageDependencies: [Package.Dependency] = [
     .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.18.7"),
     .package(url: "https://github.com/theleftbit/BSWFoundation.git", from: "7.3.2"),
     .package(url: "https://github.com/kean/Nuke.git", from: "12.8.0"),
+    .package(url: "https://source.skip.tools/skip.git", from: "1.8.0"),
+    .package(url: "https://source.skip.tools/skip-fuse-ui.git", from: "1.14.2"),
 ]
-
-if skipIsEnabled {
-    packageDependencies.append(contentsOf: [
-        .package(url: "https://source.skip.tools/skip.git", from: "1.8.0"),
-        .package(url: "https://source.skip.tools/skip-fuse-ui.git", from: "1.14.2"),
-    ])
-}
 
 var targetDependencies: [Target.Dependency] = [
     .product(name: "Nuke", package: "Nuke", condition: applePlatforms),
     .product(name: "NukeExtensions", package: "Nuke", condition: applePlatforms),
     .product(name: "NukeUI", package: "Nuke", condition: applePlatforms),
     "BSWInterfaceKitObjC",
-    "BSWFoundation"
+    "BSWFoundation",
+    .product(name: "SkipFuseUI", package: "skip-fuse-ui"),
 ]
 
-if skipIsEnabled {
-    targetDependencies.append(contentsOf: [
-        .product(name: "SkipFuseUI", package: "skip-fuse-ui"),
-    ])
-}
-
-var plugins: [Target.PluginUsage] = [ ]
-if skipIsEnabled {
-    plugins.append(
-        .plugin(name: "skipstone", package: "skip")
-    )
-}
+let plugins: [Target.PluginUsage] = [
+    .plugin(name: "skipstone", package: "skip")
+]
 
 let package = Package(
     name: "BSWInterfaceKit",
